3315d6ef816ba8270224344bc0d04b5d9b08debe
Kyle <khartson2017@gmail.com>
2021-11-28 16:15:09 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return  string[0].toUpperCase() + string.slice(1); 
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, "") 
+  }
+
+  static titleize(string) {
+    let excluded = {
+      'a': 'a', 
+      'an': 'an',
+      'but': 'but',
+      'of': 'of',
+      'and': 'and',
+      'for': 'for',
+      'at': 'at',
+      'by': 'by',
+      'from': 'from', 
+      'the': 'the'
+    };
+    let str = string.toLowerCase().split(' '); 
+    str.forEach( (word, index)  => (excluded[word] == word) ? 
+    str[index] = word : str[index] = word[0].toUpperCase() + word.slice(1) )
+    str[0] = str[0][0].toUpperCase() + str[0].slice(1);
+    return str.join(' ');
+  }
+
 }
